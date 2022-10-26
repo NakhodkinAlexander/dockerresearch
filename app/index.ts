5ddce79c3fd580057af380c204db3f65133d33cc
Sasha Nakhodkin <sasha@decisions.com>
2022-10-26 22:33:28 +0300

    Connect to MongoDB

--- app/index.ts
+++ app/index.ts
@@ -2,13 +2,17 @@ import * as mongodb from 'mongodb';
 import * as express from 'express'
 
 const app = express();
+const mongoUrl = "mongodb://mongo:27017";
+const client = new mongodb.MongoClient(mongoUrl);
 
-app.get('*', (_req, res) => {
-    const response = '<div>ABC</div>'
+app.get('*', async (_req, res) => {
+    const mongoClient = client.connect();
+    const dbsList = await (await mongoClient).db().admin().listDatabases();
+    const response = `<div>${dbsList.databases.map(db => '<br>' + db.name).join()}</div>`
 
     res.send(response);
 })
 
 app.listen(3000)
 
-console.log('Hello world')
+console.log('Ready')
